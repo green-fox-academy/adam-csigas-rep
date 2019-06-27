bcfbd40b5f8f64551b68b04ec8d26679e71a2f3a
adamcsigas <csigasadam@gmail.com>
2019-06-27 13:05:32 +0200

    lunchbreak

---
+++ week1/Thursday-javava/Workshop-exercisesjava01/hello-others.ts
@@ -0,0 +1,4 @@
+console.log("Hope you're having a nice day Pascal!");
+console.log("Hope you're having a nice day Ege!");
+console.log("Hope you're having a nice day David!");
+
---
+++ week1/Thursday-javava/Workshop-exercisesjava01/humpty-dumpty.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+
+console.log('Humpty Dumpty sat on a wall,'); //1
+console.log('Humpty Dumpty had a great fall.'); //2
+console.log('All the king\'s horses and all the king\'s men'); //3
+console.log('Couldn\'t put Humpty together again.'); //4
+
+
+
+
+
+
+
+
