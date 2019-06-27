23a7205a97253fde6bd423d41d98454cca961f16
adamcsigas <csigasadam@gmail.com>
2019-06-27 16:00:50 +0200

    backup

---
+++ week1/Thursday-javava/Workshop-exercisesjava01/define-basic-info.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string = f
+// Your age as a number = g
+// Your height in meters as a number = h
+// Whether you are married or not as a boolean = j
+
+let f : string = "Adam";
+let g : number = 3*9;
+let h : number = 181/100;
+let dids : boolean = false;
+
+console.log(f ,'\n', g ,'\n' , h , '\n', dids , '\n');
+
