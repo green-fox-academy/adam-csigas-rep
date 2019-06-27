c3c439154a9920cde3d11fd8874792aa7b9f28f8
adamcsigas <csigasadam@gmail.com>
2019-06-27 15:39:56 +0200

    atbmi

---
+++ week1/Thursday-javava/Workshop-exercisesjava01/BMI.ts
@@ -0,0 +1,9 @@
+
+// Print the Body mass index (BMI) based on these values
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+let BMI : number =((massInKg)/(heightInM*heightInM));
+
+console.log("Your BMI is " + BMI + " . You're kinda fat.") ;
---
+++ week1/Thursday-javava/Workshop-exercisesjava01/swap.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+// Swap the values of these variables
+// logic went home to sleep :D
+
+let myNumbera: number = 123;
+let myNumberb: number = 526;
+
+
+if (myNumbera < 122) {
+    console.log(myNumbera);
+}else {
+    console.log(myNumberb);
+}
+if (myNumberb < 122) {
+    console.log(myNumberb);
+}else {
+    console.log(myNumbera);
+}
+    
+console.log(myNumbera);
+console.log(myNumberb);
