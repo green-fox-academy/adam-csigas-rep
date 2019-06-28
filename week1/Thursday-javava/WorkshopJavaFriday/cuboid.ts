a3f1efc637c493ee28331455037fd17a1e3282ac
adamcsigas <csigasadam@gmail.com>
2019-06-28 11:42:11 +0200

    hardAF

---
+++ week1/Thursday-javava/WorkshopJavaFriday/cuboid.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+let A : number = 10;
+let B : number = 20;
+let C : number = 30;
+
+console.log('Surface Area: ', A*B*2+A*C*2+B*C*2 ,'\n' , 'Volume: ' , A*B*C);
