fdb5a4140d9787959423c30cb4215bc5ef49998d
adamcsigas <csigasadam@gmail.com>
2019-07-01 10:58:53 +0200

    mat-rev-monday

---
+++ week-2/day-1/material review/mat-review.ts
@@ -0,0 +1,23 @@
+/* Last week stuff   */
+
+'use strict';
+let myName : string = "Adam0";
+let yourName : string = "Adam1";
+let hisName : string = "Adam2";
+
+
+console.log(myName);
+console.log(yourName);
+console.log(hisName);
+
+let array: string[] = ["Morzsi", "Liza", "Furkesz"];
+
+console.log(array[0]);
+console.log(array);
+
+array.forEach(function(item) {
+    console.log(item);
+})
+
+
+
