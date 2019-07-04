fcf1f4ccba40c6e5cd0925474f45d0edf63f9e8d
adamcsigas <csigasadam@gmail.com>
2019-07-04 23:26:55 +0200

    re-org-plus-function-task

--- week-2/day-1/sum-fun.ts
+++
@@ -1,4 +0,0 @@
-// Write a function called `sum` that returns the sum of numbers from zero to the given parameter
-
-'use strict';
-
---
+++ week-2/day-4/w2d2-function-summing.ts
@@ -0,0 +1,45 @@
+// Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+
+'use strict';
+
+export = {};
+
+// Where N = the number of elements in our array
+
+// without function
+
+/* let myNumbers: number[] = [];
+let N: number = 10;
+let sum: number = 0;
+
+for (let i: number = 0; i <= N; i++) {
+    myNumbers.push(i);
+}
+
+console.log(myNumbers);
+
+
+for (let j: number = 0; j <= N; j++) {
+    sum += myNumbers[j];
+}
+console.log(sum); */
+
+
+// with function
+
+let myNumbers: number[] = [];
+let sum: number = 0;
+
+function sumThatArray(N: number): number {
+    for (let i: number = 0; i <= N; i++) {
+        myNumbers.push(i);
+    }
+
+    for (let j: number = 0; j <= N; j++) {
+        sum += myNumbers[j];
+    }
+
+    return sum
+}
+
+console.log(sumThatArray(20))
