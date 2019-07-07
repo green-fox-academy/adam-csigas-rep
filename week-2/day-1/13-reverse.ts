36b2d633815f4bfaa65d372a9fa06f679a020dd0
adamcsigas <csigasadam@gmail.com>
2019-07-07 20:11:37 +0200

    inWeek-2-Day1-13-reverse-glitchfixed

--- week-2/day-1/13-reverse.ts
+++ week-2/day-1/13-reverse.ts
@@ -14,13 +14,18 @@ export = {};
 
 //second one:
 
-let numList: number[] = [3, 4, 5, 6, 7];
+let numList: number[] = [3, 4, 66, 6, 7];
 let reverseNum: number[] = [];
 
-for (let i: number = numList[numList.length - 1]; i >= numList[0]; i--) {
-    reverseNum.push(i);
+for (let i: number = 1; i <= numList.length; i++) {
+    reverseNum.push(numList[numList.length - i]);
 }
 console.log(reverseNum);
+
+/* for (let i: number = numList[numList.length - 1]; i >= numList[0]; i--) {
+    reverseNum.push(i);
+}
+console.log(reverseNum); */
 
 //04.07.19 :D
 // If you're reading this, you're breathtaking!
