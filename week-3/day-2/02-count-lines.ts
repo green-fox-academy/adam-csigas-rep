66767f313223edad43b652bc861df9e418492fe6
adamcsigas <csigasadam@gmail.com>
2019-07-09 12:41:34 +0200

    w3d2-countlines

---
+++ week-3/day-2/02-count-lines.ts
@@ -0,0 +1,26 @@
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+'use strict';
+
+export { };
+
+declare function require(path: string): any;
+
+const fs = require('fs');
+
+let content;
+
+try {
+    content = fs.readFileSync('02-file.txt', 'utf-8');
+    console.log(typeof content);
+} catch (e) {
+    content = '';
+}
+
+console.log(content)
+
+let lines: string[] = content.split('\n');
+console.log(lines.length);
