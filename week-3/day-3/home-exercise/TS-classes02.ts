056660e1d8f7a1064d1884b5a7cc8b897f1829e5
adamcsigas <csigasadam@gmail.com>
2019-07-10 07:05:44 +0200

    homeprepwork-constructor-private

--- week-3/day-3/home-exercise/TS-classes02.ts
+++ week-3/day-3/home-exercise/TS-classes02.ts
@@ -6,6 +6,8 @@ class Car {
 
     private _seriesNumber: number;
 
+    private _ownerName: string;
+
     constructor(public brandName: string) {
         Car.SERIES_NUMBER++;
         this._seriesNumber = Car.SERIES_NUMBER;
@@ -21,16 +23,28 @@ class Car {
     public get seriesNumber(): number {
         return this._seriesNumber;
     }
+
+    public get ownerName(): string {
+        return this._ownerName;
+    }
+
+    public set ownerName(name: string) {
+        // Do some validation on the name
+        this._ownerName = name;
+    }
 }
 
 let car = new Car('Mazda');
 car.startCar();
+car.ownerName = 'Slava';
 console.log('Brand number:', car.brandName);
 console.log('Series number:', car.seriesNumber);
+console.log('Owner name:', car.ownerName);
 
 let car2 = new Car('Mazda');
 console.log('Brand number 2:', car2.brandName);
 console.log('Series number 2:', car2.seriesNumber);
+console.log('Owner name:', car2.ownerName);
 
 
 /* Shortcut in code01:
