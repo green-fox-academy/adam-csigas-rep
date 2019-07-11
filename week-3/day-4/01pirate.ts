9973cf3b01772510f4f01f08ff49cbb2fbee44c3
adamcsigas <csigasadam@gmail.com>
2019-07-11 16:44:31 +0200

    w3d4diceset

--- week-3/day-4/01pirate.ts
+++ week-3/day-4/01pirate.ts
@@ -1,14 +1,13 @@
 /* Create a Pirate class.While you can add other fields and methods, you must have these methods: -
 
-    drinkSomeRum() - intoxicates the Pirate some
-howsItGoingMate() - when called, the Pirate replies, if drinkSomeRun was called: -
-    0 to 4 times, "Pour me anudder!"
-else, "Arghh, I'ma Pirate. How d'ya d'ink its goin?", the pirate passes out and sleeps it off.
-If you manage to get this far, then you can try to do the following. - die() - this kills off the pirate, in which case, DrinkSomeRum, etc.just result in he's dead. - brawl(x) - where pirate fights another pirate (if that other pirate is alive) and there's a 1 / 3 chance, 1 dies, the other dies or they both pass out.
+    - die() - this kills off the pirate, in which case, DrinkSomeRum, etc.just result in he's dead. - brawl(x) - where pirate fights another pirate (if that other pirate is alive) and there's a 1 / 3 chance, 1 dies, the other dies or they both pass out.
 
     And... if you get that far...
 
 Add a parrot. */
+
+'use strict';
+export { };
 
 class Pirate {
 
@@ -27,7 +26,9 @@ class Pirate {
         //if-else state : either tell you "Pour me anudder!" or "Arghh, I'ma Pirate. How d'ya d'ink its goin?" <- with this he passes out drink goes off
     }
 
-    die() {
+    die(): Pirate {
+
+        return
         //this kills the pirate
     }
     brawl() {
@@ -49,3 +50,6 @@ Pirate1.howsItGoingMate();
 console.log(Pirate1);
 Pirate1.drinkSomeRum();
 Pirate1.howsItGoingMate();
+
+Pirate1 = undefined;
+console.log(Pirate1)
