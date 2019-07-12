cec79f6627c2616e79c66c8ca5b0b0d9264c4505
adamcsigas <csigasadam@gmail.com>
2019-07-12 12:06:00 +0200

    w3d5adder

---
+++ week-3/day-5/01-recursive-adder.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+function adder(n: number) {
+    if (n === 0) {
+        return n = 0;
+    } else {
+        return n + adder(n - 1);
+    }
+}
+
+console.log(adder(5));
