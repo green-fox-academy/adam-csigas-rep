90566105f860d8677d1ecf6bb61d3fa7795c6375
adamcsigas <csigasadam@gmail.com>
2019-07-14 19:22:14 +0200

    w4d1prepworkmeh

--- week-4/day-1/homeprep/01-classinheritance.ts
+++ week-4/day-1/homeprep/01-classinheritance.ts
@@ -2,10 +2,23 @@ class Person {
     constructor(name) {
         console.log(name + ' Person constructor');
     }
+    getID() {
+        return 10;
+    }
 }
 
 class Employee extends Person {
-
+    constructor(name) {
+        super(name);
+        console.log(name + " Employee constructor");
+    }
+    getID() {
+        return super.getID();
+    }
 }
 
 let e = new Employee("Adam");
+console.log(e.getID());
+
+// if you do 2 classes, A,B and eg. in to the B one you put extends + A class' name, B will "inherit" the A's stuff, like constructor
+// and constructors parameter
