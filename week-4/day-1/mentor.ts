5c2d744fca761a2c6009ef1ba9d3d4b00558ae5a
adamcsigas <csigasadam@gmail.com>
2019-07-15 14:45:24 +0200

    sponsor-ready

--- week-4/day-1/mentor.ts
+++ week-4/day-1/mentor.ts
@@ -8,4 +8,20 @@ class Mentor extends Person {
         super(name, age, gender);
         this.level = level;
     }
+
+    getGoal() {
+        console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} ${this.level} mentor.`)
+    }
+
+    introduce() {
+        console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} ${this.level} mentor.`)
+    }
 }
+
+/*
+fields:
+level: the level of the mentor (junior / intermediate / senior)
+methods:
+getGoal(): prints out "Educate brilliant junior software developers."
+introduce(): "Hi, I'm name, a age year old gender level mentor."
+*/
---
+++ week-4/day-1/sponsor.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+import Person from './person';
+
+class Sponsor extends Person {
+    company: string;
+    hiredStudents: number;
+
+    constructor(name?: string, age?: number, gender?: string, company = 'Google', hiredStudents = 0) {
+
+        super(name, age, gender);
+
+        this.company = company;
+        this.hiredStudents = hiredStudents;
+    }
+
+    introduce() {
+        console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} who represents ${this.company} and hired ${this.hiredStudents} students so far.`)
+    }
+
+    hire() {
+        this.hiredStudents++;
+    }
+
+    getGoal() {
+        console.log("Hire brilliant junior software developers.");
+    }
+
+}
