e6e43342560233f98d91ac1f83ed647f0430b76c
adamcsigas <csigasadam@gmail.com>
2019-07-22 23:13:57 +0200

    w1d5-diagonal-littleclean

--- week-off-exercises/w1d5-diagonal.ts
+++ week-off-exercises/w1d5-diagonal.ts
@@ -8,18 +8,5 @@ for (let i: number = 0; i <= lineCount; i++) {
     } else if (i < lineCount - 1) {
         console.log(Array(2).join('%') + Array(i).join(' ') + Array(2).join('%') + Array(lineCount - i - 1
         ).join(' ') + Array(2).join('%'));
-        //console.log(Array(lineCount - lineCount + 2 + i).join('%') + Array(lineCount - 1).join(' ') + Array(lineCount - lineCount + 2).join('%'));
     }
 }
-
-// Write a program that draws a
-// square like this:
-//
-// %%%%%%
-// %%   %
-// % %  %
-// %  % %
-// %   %%
-// %%%%%%
-//
-// The square should have as many lines as lineCount is
