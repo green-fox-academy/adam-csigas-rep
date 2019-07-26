29c3f23f14f3d958c3d7bfd9f95fbc864ae3f9f1
adamcsigas <csigasadam@gmail.com>
2019-07-26 12:04:48 +0200

    testing-randomPirate-success02

--- week-off-exercises/test-pirate.ts
+++ week-off-exercises/test-pirate.ts
@@ -9,9 +9,14 @@ test(t => {
     t.equals(pirate01.gold, 10);
     t.equals(pirate01.health, 10);
     if (pirate01.isCaptain) {
-        t.equals(pirate01.isCaptain, true)
+        t.equals(pirate01.isCaptain, true);
     } else {
         t.equals(pirate01.isCaptain, false);
+    }
+    if (pirate01.isLegWooden) {
+        t.equals(pirate01.isLegWooden, true);
+    } else {
+        t.equals(pirate01.isLegWooden, false);
     }
     //t.equals(pirate01.isLegWooden, true);
     //t.equals(pirate01.isLegWooden, false);
