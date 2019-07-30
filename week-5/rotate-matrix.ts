6c2b1c7714e308c83d62bbf588741c0eb9fcf80a
adamcsigas <csigasadam@gmail.com>
2019-07-30 09:34:14 +0200

    MatrixSkeleton01

---
+++ week-5/rotate-matrix.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+let myMatress: number[][] = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+for (let i: number = 0; i < myMatress.length; i++) {
+    for (let j: number = 0; j < myMatress.length; j++) {
+
+    }
+}
