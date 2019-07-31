e0e406a163cb156c19e8f1e0e3c789fabb5e93d3
adamcsigas <csigasadam@gmail.com>
2019-07-31 14:59:20 +0200

    ImplementIdea

--- week-5/playground.ts
+++ week-5/playground.ts
@@ -3,14 +3,23 @@
 //
 let mystring: string = 'k.oc.ka.krum.pli.';
 let myDestroy: any[] = ['k', /\./gi];
-//let myNewstring02: string = '';
 
-/* for (let i: number = 0; i < myDestroy.length; i++) {
+
+function deleteCharacter(): string {
+    let myNewstring02: string = '';
+
+    return myNewstring02;
+}
+
+for (let i: number = 0; i < myDestroy.length; i++) {
     mystring.replace(myDestroy[i], "");
-} */
+}
 
-let myNewstring: string = mystring.replace(myDestroy[0], "");
+
+
+
+/* let myNewstring: string = mystring.replace(myDestroy[0], "");
 let myNewstring2: string = mystring.replace(myDestroy[1], "");
 
-console.log(myNewstring2)
+console.log(myNewstring2) */
 
