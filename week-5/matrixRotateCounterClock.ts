fc6272bc333762e3465ea5d3d9050770436dd3e0
adamcsigas <csigasadam@gmail.com>
2019-08-01 17:43:10 +0200

    LastToday

---
+++ week-5/matrixRotateCounterClock.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+let myMatrix : number[][] = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+let myRotatedMatrix : number [][]= [];
+
+for (let i: number = myMatrix.length - 1; i >= 0; i--){
+    let row : number[] = [];
+    for(let j : number = 0; j < myMatrix.length; j++){
+        row.push(myMatrix[j][i]);
+    }
+    myRotatedMatrix.push(row);
+}
+
+console.log(myRotatedMatrix)
