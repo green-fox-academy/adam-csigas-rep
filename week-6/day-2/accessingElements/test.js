259c7277be1434fe2f61d7735796eef38139672d
adamcsigas <csigasadam@gmail.com>
2019-08-06 14:22:45 +0200

    its-something

---
+++ week-6/day-2/accessingElements/test.js
@@ -0,0 +1,9 @@
+'use strict';
+let king = document.getElementById('b325');
+console.log('king', king);
+let lamplighter = document.querySelector('.b329');
+console.log('lamplighter', lamplighter);
+let asteroids = document.querySelectorAll('div.asteroid');
+for (let i = 0; i < asteroids.length; i++) {
+    console.log(asteroids[i]);
+}
