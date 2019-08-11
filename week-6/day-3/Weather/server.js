6228e62a308edf59a7c1b34c51286793713c5925
adamcsigas <csigasadam@gmail.com>
2019-08-11 12:11:55 +0200

    Weather App icons working

--- week-6/day-3/Weather/server.js
+++ week-6/day-3/Weather/server.js
@@ -2,66 +2,68 @@ const express = require('express');
 const app = express();
 const PORT = 3010;
 
+app.use(express.static('static'));
+app.use(express.static('images'));
+
 const forecasts = [
     {
         city: 'Seattle',
         location: 'Washington, United States',
         weather: [
-            { temp: 11, icon: 'cloudy', message: 'Complete Grey' },
-            { temp: 11, icon: 'cloudy', message: 'Just stay in bed.' },
-            { temp: 13, icon: 'cloudy', message: 'Meh...' },
-            { temp: 12, icon: 'cloudy', message: 'Still meh...' },
-            { temp: 13, icon: 'partly_cloudy', message: 'Not too sunny.' },
+            { temp: 11, icon: 'cloudy.png', message: 'Complete Grey' },
+            { temp: 11, icon: 'cloudy.png', message: 'Just stay in bed.' },
+            { temp: 13, icon: 'cloudy.png', message: 'Meh...' },
+            { temp: 12, icon: 'cloudy.png', message: 'Still meh...' },
+            { temp: 13, icon: 'partly_cloudy.png', message: 'Not too sunny.png.' },
         ],
     },
     {
         city: 'Miami',
         location: 'Florida, United States',
         weather: [
-            { temp: 33, icon: 'sunny', message: 'Hot.' },
-            { temp: 35, icon: 'sunny', message: 'Too hot!' },
-            { temp: 34, icon: 'sunny', message: 'Sunny.' },
-            { temp: 34, icon: 'sunny', message: 'Beach time!' },
-            { temp: 35, icon: 'sunny', message: 'Here comes the sun.' },
+            { temp: 33, icon: 'sunny.png', message: 'Hot.' },
+            { temp: 35, icon: 'sunny.png', message: 'Too hot!' },
+            { temp: 34, icon: 'sunny.png', message: 'Sunny.' },
+            { temp: 34, icon: 'sunny.png', message: 'Beach time!' },
+            { temp: 35, icon: 'sunny.png', message: 'Here comes the sun.' },
         ],
     },
     {
         city: 'Barcelona',
         location: 'Spain',
         weather: [
-            { temp: 19, icon: 'sunny', message: 'Sunny.' },
-            { temp: 15, icon: 'partly_cloudy', message: 'Not too sunny.' },
-            { temp: 17, icon: 'sunny', message: 'So far so good.' },
-            { temp: 16, icon: 'rainy', message: 'Rihanna - Umbrella' },
-            { temp: 18, icon: 'sunny', message: 'Here comes the sun.' },
+            { temp: 19, icon: 'sunny.png', message: 'Sunny.' },
+            { temp: 15, icon: 'partly_cloudy.png', message: 'Not too sunny.png.' },
+            { temp: 17, icon: 'sunny.png', message: 'So far so good.' },
+            { temp: 16, icon: 'rainy.png', message: 'Rihanna - Umbrella' },
+            { temp: 18, icon: 'sunny.png', message: 'Here comes the sun.' },
         ],
     },
     {
         city: 'London',
         location: 'United Kingdom',
         weather: [
-            { temp: 4, icon: 'snowy', message: 'Go home winter.' },
-            { temp: 7, icon: 'rainy', message: 'Do you have an umbrella?' },
-            { temp: 10, icon: 'rainy', message: 'It\'s rainy.' },
-            { temp: 9, icon: 'rainy', message: 'Meh...' },
-            { temp: 11, icon: 'rainy', message: 'Meh... Again.' },
+            { temp: 4, icon: 'snowy.png', message: 'Go home winter.' },
+            { temp: 7, icon: 'rainy.png', message: 'Do you have an umbrella?' },
+            { temp: 10, icon: 'rainy.png', message: 'It\'s rainy.png.' },
+            { temp: 9, icon: 'rainy.png', message: 'Meh...' },
+            { temp: 11, icon: 'rainy.png', message: 'Meh... Again.' },
         ],
     },
     {
         city: 'Budapest',
         location: 'Hungary',
         weather: [
-            { temp: 12, icon: 'partly_cloudy', message: 'It\'s cloudy.' },
-            { temp: 16, icon: 'rainy', message: 'Sooo... Wet.' },
-            { temp: 18, icon: 'partly_cloudy', message: 'Here comes the sun.' },
-            { temp: 16, icon: 'partly_cloudy', message: 'Not too sunny.' },
-            { temp: 19, icon: 'partly_cloudy', message: 'So far so good.' },
+            { temp: 12, icon: 'partly_cloudy.png', message: 'It\'s cloudy.png.' },
+            { temp: 16, icon: 'rainy.png', message: 'Sooo... Wet.' },
+            { temp: 18, icon: 'partly_cloudy.png', message: 'Here comes the sun.' },
+            { temp: 16, icon: 'partly_cloudy.png', message: 'Not too sunny.png.' },
+            { temp: 19, icon: 'partly_cloudy.png', message: 'So far so good.' },
         ],
     },
 ];
 
 app.set('view engine', 'ejs');
-app.use(express.static('images'));
 
 // home page
 app.get('/', (req, res) => {
