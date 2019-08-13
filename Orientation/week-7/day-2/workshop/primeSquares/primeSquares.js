d1153acfb2af0f933cf462d32b75b8409f5e0512
adamcsigas <csigasadam@gmail.com>
2019-08-13 14:48:42 +0200

    primaresquares

---
+++ Orientation/week-7/day-2/workshop/primeSquares/primeSquares.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* generate 100 divs to the < section > element and add index numbers to it as the element's textContent
+Create a function that adds a 'not-prime' class to a div if it's not a prime and 'prime' if it is
+Create a timer that keeps calling the prime validator function until it reaches the last element
+the timer should fire every 100ms
+the timer should stop when there are no more elements left to be colored */
+
