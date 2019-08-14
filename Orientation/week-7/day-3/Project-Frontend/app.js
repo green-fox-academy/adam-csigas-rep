5bd67b4ba9507d005e88870d0b31f72777e39c6e
adamcsigas <csigasadam@gmail.com>
2019-08-14 16:46:00 +0200

    dountil omg

--- Orientation/week-7/day-3/Project-Frontend/app.js
+++ Orientation/week-7/day-3/Project-Frontend/app.js
@@ -23,7 +23,45 @@ app.get('/doubling', (req, res) => {
             "result": Number(req.query.input * 2)
         })
     }
+});
 
+app.get('/greeter', (req, res) => {
+
+    if (req.query.name === undefined && req.query.name === undefined) {
+        res.json({
+            "error": "Please provide a name and a title!"
+        });
+    } else if (req.query.name === undefined) {
+        res.json({
+            "error": "Please provide a name!"
+        });
+    } else if (req.query.title === undefined) {
+        res.json({
+            "error": "Please provide a title!"
+        });
+    } else {
+        res.json({
+            "welcome_message": "Oh, hi there " + req.query.name + ", my dear " + req.query.title + "!"
+        });
+    }
+});
+
+app.get('/appenda/:word', (req, res) => {
+    if (req.params.word === undefined) {
+        res.status(404);
+    } else {
+        res.json({
+            "appended": req.params.word + "a"
+        })
+    }
+});
+
+app.post('/dountil/:action', (req, res) => {
+    if (req.params.action === "sum") {
+        res.json({
+            "result": (req.body.until*(req.body.until+1))/2
+        })
+    }
 });
 
 
