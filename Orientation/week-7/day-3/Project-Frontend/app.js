deeb431bd990ab16764067feacc90a27bdafd506
adamcsigas <csigasadam@gmail.com>
2019-08-14 13:15:52 +0200

    it doubles not doubled doubables

--- Orientation/week-7/day-3/Project-Frontend/app.js
+++ Orientation/week-7/day-3/Project-Frontend/app.js
@@ -1,12 +1,33 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+
+const app = express();
 const PORT = 3000;
+
 app.use(express.static('assets'));
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+app.get('/doubling', (req, res) => {
+    
+    res.json({
+        "received": Number(req.query.input),
+        "result": Number(req.query.input * 2)
+    })
+    
+});
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`The server is up and running on ${PORT}`);
+});
