ec9f875910e6a9fdd70d1c58d329076465f28c77
adamcsigas <csigasadam@gmail.com>
2019-08-22 11:45:47 +0200

    W08-D03 anagram stuff and random generator

---
+++ Orientation/week-8/Dojo/anagram.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// silent and listen are anagrams
+
+let myword1 : string = 'listen';
+let myword2 : string = 'silent';
+let outPut : boolean = false;
+
+if(myword1.length === myword2.length){
+    outPut = true;
+}
+
+console.log(outPut)
+
+
+myword1.length
+myword2.length 
