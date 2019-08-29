020c7b77c8baad2b0a6517143b69f2bc6e689000
adamcsigas <csigasadam@gmail.com>
2019-08-30 00:16:10 +0200

    callback-1 exercise 1

---
+++ Orientation/weekoff-exercises/Callback-w7d2/callback-1.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const mapWith = (array, callback) => {
+    let output = [];
+    array.forEach(element => {
+        output.push(callback(element));
+    });
+    return output;
+}
+
+const addOne = (number) => {
+    return number + 1;
+}
+
+// Exercise 1:
+
+console.log(mapWith([1, 2, 3], addOne));
+//expected result: [2, 3, 4]
+
+// Exercise 2:
+
+// Create a callback function which removes every second character from a string
+
+const words = ['map', 'reduce', 'filter'];
+
+//console.log(mapWith(words, removeSecondLetter));
+// expected result: ['mp','rdc', 'fle']
