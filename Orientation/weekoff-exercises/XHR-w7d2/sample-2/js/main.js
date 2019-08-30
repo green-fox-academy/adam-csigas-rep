35c84a79a38e57ee72ea7b3d2188d770190c88a9
adamcsigas <csigasadam@gmail.com>
2019-08-30 16:02:48 +0200

    sample task 2 works

--- Orientation/weekoff-exercises/XHR-w7d2/sample-2/js/main.js
+++ Orientation/weekoff-exercises/XHR-w7d2/sample-2/js/main.js
@@ -1,9 +1,43 @@
 'use strict';
 
-let ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-ourRequest.onload = function() {
-    let ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData[0]);
-};
-ourRequest.send();
+let pageCounter = 1;
+let animalContainer = document.getElementById("animal-info");
+let btn = document.getElementById("btn");
+
+let myReq = function () {
+    let ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`);
+
+    ourRequest.onload = function () {
+        if (ourRequest.status >= 200 && ourRequest.status < 400) {
+            let ourData = JSON.parse(ourRequest.responseText);
+            renderHTML(ourData);
+        } else {
+            console.log("something went wrong Captain!");
+        }
+    };
+
+    ourRequest.send();
+    pageCounter++;
+    if (pageCounter > 3) {
+        btn.removeEventListener("click", myReq);
+    }
+}
+
+btn.addEventListener("click", myReq);
+
+function renderHTML(data) {
+    let htmlString = '';
+    for (let i = 0; i < data.length; i++) {
+        htmlString += '<p>' + data[i].name + `is a ${data[i].species}` + ' that likes '
+        for (let j = 0; j < data[i].foods.likes.length; j++) {
+            if (j == 0) {
+                htmlString += data[i].foods.likes[j];
+            } else {
+                htmlString += ` and ${data[i].foods.likes[j]}`;
+            }
+        }
+        htmlString += '.</p>';
+    }
+    animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
