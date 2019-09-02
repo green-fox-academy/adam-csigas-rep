b8383f148aa6ba9778425d6dea893bcd70b8dded
adamcsigas <csigasadam@gmail.com>
2019-09-02 14:34:29 +0200

    promise shortcuts

--- Orientation/week-9/day-1/promise/program.js
+++ Orientation/week-9/day-1/promise/program.js
@@ -1,8 +1,11 @@
 'use strict';
 
-let promise = new Promise(function (resolve, reject) {
-    resolve('PROMISE VALUE');
-})
+let promise1 = Promise.resolve('SECRET VALUE1');
+let promise2 = Promise.reject(new Error('SECRET VALUE2'));
 
-promise.then(console.log);
-console.log('MAIN PROGRAM');
+let errorcatcher = function(error){
+  console.log(error.message);
+}
+
+promise1.then(console.log);
+promise2.catch(errorcatcher);
