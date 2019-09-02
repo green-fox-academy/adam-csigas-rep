169827c3d3e22bb9d52ea9c8803f64c7ec403016
adamcsigas <csigasadam@gmail.com>
2019-09-02 15:14:05 +0200

    random chuck norris joke

---
+++ Orientation/week-9/day-1/chuck-norris/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const URL = 'http://api.icndb.com/jokes/random';
+let container = document.querySelector('div');
+let btn = document.querySelector('button');
+
+const randomJoke = function(){
+    const paragraph = document.createElement("p");
+    fetch(URL)
+    .then(response => response.json())
+    .then(myJson => 
+        paragraph.innerHTML = `${myJson.value.joke}`);
+        container.appendChild(paragraph)
+}
+
+btn.addEventListener('click',randomJoke);
