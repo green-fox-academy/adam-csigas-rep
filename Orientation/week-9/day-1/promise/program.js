8af5f5b7b0198abe690e1ae7a0d55f8010f55c01
adamcsigas <csigasadam@gmail.com>
2019-09-02 12:49:54 +0200

    To reject or not to reject

--- Orientation/week-9/day-1/promise/program.js
+++ Orientation/week-9/day-1/promise/program.js
@@ -1,19 +1,12 @@
 'use strict';
 
-let promise = new Promise(function(resolve,reject){
-  setTimeout(() => reject(new Error('REJECTED!')), 300);
+let promise = new Promise(function (resolve, reject) {
+    resolve('I FIRED');
+    reject(new Error('I DID NOT FIRE'));
 })
 
-const onReject = function(error){
-  console.log(error.message)
+const onRejected = function (error) {
+    console.log(error.message);
 }
 
-promise.then(null,onReject);
-
-/* 
-It works the same : 
-promise.catch(onReject);
-
-promise.then(console.log)
-.catch(onReject)
-*/
+promise.then(console.log, onRejected);
