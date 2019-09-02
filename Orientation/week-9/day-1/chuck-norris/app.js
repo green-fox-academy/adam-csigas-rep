b87eff972e00dc343a77b9ec3722de5c0e7c175d
adamcsigas <csigasadam@gmail.com>
2019-09-02 15:22:27 +0200

    chuck norris

--- Orientation/week-9/day-1/chuck-norris/app.js
+++ Orientation/week-9/day-1/chuck-norris/app.js
@@ -4,13 +4,13 @@ const URL = 'http://api.icndb.com/jokes/random';
 let container = document.querySelector('div');
 let btn = document.querySelector('button');
 
-const randomJoke = function(){
+const randomJoke = function () {
     const paragraph = document.createElement("p");
     fetch(URL)
-    .then(response => response.json())
-    .then(myJson => 
-        paragraph.innerHTML = `${myJson.value.joke}`);
-        container.appendChild(paragraph)
+        .then(response => response.json())
+        .then(myJson =>
+            paragraph.innerHTML = `${myJson.value.joke}`);
+    container.appendChild(paragraph)
 }
 
-btn.addEventListener('click',randomJoke);
+btn.addEventListener("click",randomJoke);
