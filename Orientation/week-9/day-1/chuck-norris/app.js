89ee3b5c1be5e6ac45f796eaf6451b250a33cda0
adamcsigas <csigasadam@gmail.com>
2019-09-02 16:22:57 +0200

    chuck norris gives only 5 jokes

--- Orientation/week-9/day-1/chuck-norris/app.js
+++ Orientation/week-9/day-1/chuck-norris/app.js
@@ -3,14 +3,21 @@
 const URL = 'http://api.icndb.com/jokes/random';
 let container = document.querySelector('div');
 let btn = document.querySelector('button');
+let counter = 0;
 
-const paragraph = document.createElement("p");
 const randomJoke = function () {
+    const paragraph = document.createElement("p");
     fetch(URL)
         .then(response => response.json())
         .then(myJson =>
             paragraph.innerHTML = `${myJson.value.joke}`);
     container.appendChild(paragraph)
+    if(counter > 5){
+        btn.removeEventListener('click',randomJoke);
+        console.log("NO MORE JOKES TO YOU!")
+    }
+    counter++;
 }
+btn.addEventListener("click", randomJoke);
 
-btn.addEventListener("click",randomJoke);
+
