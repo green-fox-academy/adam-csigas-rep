e8fe543bd001a36656b72698d50b41d82c2d4b8a
adamcsigas <csigasadam@gmail.com>
2019-09-04 14:49:36 +0200

    yondu is on the go

--- Orientation/week-9/day-3/iamgroot/routes.js
+++ Orientation/week-9/day-3/iamgroot/routes.js
@@ -3,15 +3,38 @@ const app = express();
 
 app.get('/groot', (req, res) => {
     if (req.query.somemessage === undefined) {
-        res.json({
-            "error": "I am Groot!"
-        })
+        res.status(400)
+            .json({
+                "error": "I am Groot!"
+            })
+
     } else {
         res.json({
-            "received": "somemessage",
+            "received": req.query.somemessage,
             "translated": "I am Groot!"
         })
     }
 });
+
+app.get('/yondu', (req, res) => {
+    if (req.query.time === "0" || 
+        req.query.distance === "0" || 
+        isNaN(Number(req.query.distance)) || 
+        isNaN(Number(req.query.time))){
+        res.send({
+            "error" : "Either given number is not correct or the given value is not a number.gittggg"
+        })
+    } else if (!isNaN(Number(req.query.time)) && !isNaN(Number(req.query.distance))){
+        res.send({
+            "distance": req.query.distance,
+            "time": req.query.time,
+            "speed": req.query.distance / req.query.time
+        })
+    }
+});
+
+module.exports = app;
+
+
 
 module.exports = app;
