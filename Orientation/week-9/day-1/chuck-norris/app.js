c34cf2c967111e93217ef88d3a466f01fc4f3531
adamcsigas <csigasadam@gmail.com>
2019-09-06 09:20:41 +0200

    last save from yesterday

--- Orientation/week-9/day-1/chuck-norris/app.js
+++ Orientation/week-9/day-1/chuck-norris/app.js
@@ -3,7 +3,7 @@
 const URL = 'http://api.icndb.com/jokes/random';
 let container = document.querySelector('div');
 let btn = document.querySelector('button');
-let counter = 0;
+let counter = 1;
 
 const randomJoke = function () {
     const paragraph = document.createElement("p");
@@ -12,7 +12,7 @@ const randomJoke = function () {
         .then(myJson =>
             paragraph.innerHTML = `${myJson.value.joke}`);
     container.appendChild(paragraph)
-    if(counter > 5){
+    if(counter >= 10){
         btn.removeEventListener('click',randomJoke);
         console.log("NO MORE JOKES TO YOU!")
     }
