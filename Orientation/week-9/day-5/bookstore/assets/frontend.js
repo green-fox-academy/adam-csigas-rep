38cfc8fb22b3096ced47708529930b0ee61ee02a
adamcsigas <csigasadam@gmail.com>
2019-09-08 17:47:11 +0200

    bookstore ready

--- Orientation/week-9/day-5/bookstore/assets/frontend.js
+++ Orientation/week-9/day-5/bookstore/assets/frontend.js
@@ -1,16 +1,23 @@
-function render(data){
-    data.forEach(e => {
-        let tablebody = document.querySelector('tbody');
-        let tableRow = document.createElement('tr');
-        let bookname = e.book_name;
-        tableRow.textContent= bookname;
-        tablebody.appendChild(tableRow);
-    });
-} 
+function render(data) {
+  data.forEach(e => {
+    //where
+    let tableBody = document.querySelector('tbody');
+    let tableRow = document.createElement('tr');
+    let infosOfBook = [e.book_name, e.aut_name, e.cate_descrip, e.pub_name,e.book_price];
+
+    for (let i = 0; i < infosOfBook.length; i++) {
+      let tableData = document.createElement('td');
+      tableData.textContent = infosOfBook[i];
+      tableRow.appendChild(tableData);
+    }
+    tableBody.appendChild(tableRow);
+    //what book_name,aut_name,cate_descrip,pub_name
+  });
+}
 const URL = 'http://localhost:3000';
-fetch(URL +'/books')
-    .then(response => response.json())
-    .then(render)
+fetch(URL + '/books')
+  .then(response => response.json())
+  .then(render)
 
 
 
--- Orientation/week-9/day-5/bookstore/server.js
+++ Orientation/week-9/day-5/bookstore/server.js
@@ -4,42 +4,42 @@ const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('assets'));  
+app.use(express.static('assets'));
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'bookstore'
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'bookstore'
 });
 
 //connecting to database
 connection.connect(error => {
-    if(error){
-        console.log(`cannot connect to database.`)
-        return;
-    }
-    console.log('connection established.')
+  if (error) {
+    console.log(`cannot connect to database.`)
+    return;
+  }
+  console.log('connection established.')
 })
 
 //rendering index.html
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/books', (req, res) => {
-    let bookTitles =
-    `SELECT book_name,aut_name,cate_descrip,pub_name FROM book_mast
+  let bookTitles =
+    `SELECT book_name,aut_name,cate_descrip,pub_name,book_price FROM book_mast
     JOIN author ON book_mast.aut_id = author.aut_id
     JOIN category ON book_mast.cate_id = category.cate_id
     JOIN newpublisher ON book_mast.pub_id = newpublisher.pub_id`;
-    connection.query(bookTitles, (err, rows) => {
-        res.send(rows);
-        console.log(err)
-    });
+  connection.query(bookTitles, (err, rows) => {
+    res.send(rows);
+    console.log(err)
+  });
 });
 
 
 app.listen(PORT, () => {
-    console.log(`Server is up and running on port ${PORT}`);
+  console.log(`Server is up and running on port ${PORT}`);
 });
