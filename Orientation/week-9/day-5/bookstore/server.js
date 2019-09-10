ddd6fbed8d39f0a40235cb042bc6a1f65adb0f31
adamcsigas <csigasadam@gmail.com>
2019-09-10 23:31:02 +0200

    query builder idea

--- Orientation/week-9/day-5/bookstore/server.js
+++ Orientation/week-9/day-5/bookstore/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
+const bookTit = require('./querybild');
 
 app.use(express.static('assets'));
 
@@ -30,11 +31,7 @@ app.get('/', (req, res) => {
 
 app.get('/books', (req, res) => {
   const { category } = req.query;
-  let bookTitles =
-    `SELECT book_name,aut_name,cate_descrip,pub_name,book_price FROM book_mast 
-    JOIN author ON book_mast.aut_id = author.aut_id 
-    JOIN category ON book_mast.cate_id = category.cate_id 
-    JOIN newpublisher ON book_mast.pub_id = newpublisher.pub_id`;
+  let bookTitles = bookTit();
   if (category) {
     bookTitles = `SELECT book_name,aut_name,cate_descrip,pub_name,book_price FROM book_mast 
       JOIN author ON book_mast.aut_id = author.aut_id 
@@ -42,11 +39,7 @@ app.get('/books', (req, res) => {
       JOIN newpublisher ON book_mast.pub_id = newpublisher.pub_id
       WHERE cate_descrip = ?;`;
   }else{
-    let bookTitles =
-      `SELECT book_name,aut_name,cate_descrip,pub_name,book_price FROM book_mast 
-    JOIN author ON book_mast.aut_id = author.aut_id 
-    JOIN category ON book_mast.cate_id = category.cate_id 
-    JOIN newpublisher ON book_mast.pub_id = newpublisher.pub_id`;
+    let bookTitles = bookTit();
   }
 
   connection.query(bookTitles, category, (err, rows) => {
