5a2bb306d5d4634f7becee307d80f3f0fb89393e
adamcsigas <csigasadam@gmail.com>
2019-09-22 14:06:06 +0200

    2 way databinding

--- Project/second-project/src/app/address-card/address-card.component.ts
+++ Project/second-project/src/app/address-card/address-card.component.ts
@@ -8,19 +8,18 @@ import { User } from './user.model';
 })
 export class AddressCardComponent implements OnInit {
 
-  user: any;
-  @Input('user') userObj : User;
+  isCollapsed: boolean = true;
+  @Input('user') user: User;
 
   constructor() {
 
   }
+
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
   ngOnInit() {
-    this.user = {
-      name: this.userObj.name,
-      title: this.userObj.designation,
-      address: this.userObj.address,
-      Phone: this.userObj.phone
-    };
   }
 
 }
--- Project/second-project/src/app/app.component.ts
+++ Project/second-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './address-card/user.model';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  user : User;
+  inputText : string = "Initial value";
+
+  constructor() {
+    this.user = new User();
+    this.user.name = "Foo Bar";
+    this.user.designation = " Software Engineer";
+    this.user.address = "1000 Street City State";
+    this.user.phone = [
+      "123-123-1234",
+      "this is mah number brah"
+    ];
+  }
+
 }
