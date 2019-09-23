a7fcd2c2ef547ed1d5f53ee4fcd0084936615825
adamcsigas <csigasadam@gmail.com>
2019-09-23 12:26:30 +0200

    remapping

--- Project/angularBasicsJavabrains/first-project/src/app/date/date.component.ts
+++ Project/angularBasicsJavabrains/first-project/src/app/date/date.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { setInterval } from 'timers';
 
 @Component({
   selector: 'date',
@@ -7,22 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DateComponent implements OnInit {//dynamic part
 
-  dateMessage : string;
-  someNumber : number = 10;
+  dateMessage: string;
+  someNumber: number = 10;
 
   constructor() {
-
-    setInterval(() =>{ //there is 2 setinterval one is ok here, the other cause error
+    //there is 2 setinterval one is ok here, the other cause error
+    setInterval (() => {
       let currentDate = new Date();
       this.dateMessage = `${currentDate.toDateString()} ${currentDate.toLocaleTimeString()}`;
-    },1000);
+    }, 1000);
 
-   }
+  }
 
   ngOnInit() {
   }
 
-  addTwoNumbers(a : number,b : number) : number {
-    return a+b;
+  addTwoNumbers(a: number, b: number): number {
+    return a + b;
   }
 }
