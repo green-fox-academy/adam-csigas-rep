08ca86c8ad2471efe9f8e402731374c05a31b11c
adamcsigas <csigasadam@gmail.com>
2019-09-26 13:41:04 +0200

    some implementation in weather app

--- Project/angularBasicsJavabrains/weather-app/src/app/weather/weather.component.ts
+++ Project/angularBasicsJavabrains/weather-app/src/app/weather/weather.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 
-
 @NgModule({
   imports : [
-    FormsModule,
     CommonModule,
     HttpClient
   ]
@@ -23,17 +20,25 @@ import { HttpClient } from '@angular/common/http';
 export class WeatherComponent implements OnInit {
   nameofCity: string = "";
   response : any;
+  response1 : any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.weatherInBudapest()
   }
-
+  weatherInBudapest(){
+    this.http.get('https://api.openweathermap.org/data/2.5/weather?q=Budapest&units=metric&APPID=9275b72dd799e13720c39f8c455f8de2')
+      .subscribe((response1) => {
+        this.response1 = response1;
+        //console.log(this.response);
+      })
+  }
   search() {
     this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + this.nameofCity + '&units=metric&APPID=9275b72dd799e13720c39f8c455f8de2' )
       .subscribe((response) => {
         this.response = response;
-        //console.log(this.response);
+        console.log(this.response);
       })
   }
 }
