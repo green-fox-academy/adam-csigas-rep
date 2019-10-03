4988b2a823ac5fa6f34cdf50c20a6f12c1dd218c
adamcsigas <csigasadam@gmail.com>
2019-10-03 17:53:16 +0200

    hackerrank grade

--- practice/hackerRank/grade.js
+++ practice/hackerRank/grade.js
@@ -1,16 +1,27 @@
 'use strict';
 
-let mygrades = [73, 67, 38, 33];
-
-
+let Mahgrades = [73, 67, 38, 33, 84, 29];
 
 function gradingStudents(grades) {
+  grades = grades.map(e => {
+    if (e < 38) {
+      return e;
+    }
+    if (e % 5 > 2) {
+      return e - e % 5 + 5;
+    }
+    return e;
+  })
+  return grades;
+}
+
+/* function gradingStudents(grades) {
   grades.map(e => {
     if (e % 5 > 2) {
       return e = e - e%5 + 5;
     }
   });
   return grades;
-}
+} */
 
-console.log(gradingStudents(mygrades))
+console.log(mygrades)
